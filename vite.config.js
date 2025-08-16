ce5eec3b37fc963b8f581d957bd767833d792281
Lord <toledosandro59@gmail.com>
2025-08-16 11:11:34 -0500

    config vite y eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-env node */
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      API_SECRET: JSON.stringify(env.API_SECRET)
+    }
+  };
+});
